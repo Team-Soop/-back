df1849aa91b71e7a15b661432999c686df01d54d
wjdcks2252 <wjdcks2252@naver.com>
2024-04-05 17:36:57 +0900

    feat:이메일인증 완료, OAuath2 config 셋팅 하기

--- src/main/java/com/team_soop/soop/repository/UserMapper.java
+++ src/main/java/com/team_soop/soop/repository/UserMapper.java
@@ -1,5 +1,6 @@
 package com.team_soop.soop.repository;
 
+import com.team_soop.soop.entity.RoleRegister;
 import com.team_soop.soop.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -7,6 +8,12 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface UserMapper {
     public User findUserByUsername(String username);
+
     public int saveUser(User user);
+
+    public RoleRegister findRoleRegisterByUserIdAndRoleId(@Param("userId") int userId, @Param("roleId") int roleId);
+
     public int saveRole(@Param("userId") int userId, @Param("roleId") int roleId);
+
+    public User findUserByOAuth2name(String oAuth2name);
 }

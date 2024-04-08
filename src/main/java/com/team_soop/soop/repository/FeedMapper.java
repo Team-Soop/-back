db5a7a624d7ed7c5f5d6ecdf65c020ef6181fe91
kangjy311 <kangjy311@naver.com>
2024-04-08 17:49:46 +0900

    feaf: 게시판 text, image DB에 저장

---
+++ src/main/java/com/team_soop/soop/repository/FeedMapper.java
@@ -0,0 +1,15 @@
+package com.team_soop.soop.repository;
+
+import com.team_soop.soop.entity.Feed;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface FeedMapper {
+
+    public int saveFeed(Feed feed);
+    public int saveFeedImgUrl(@Param("feedId") int feedId, @Param("feedImgUrls") List<String> feedImgUrls);
+
+}
